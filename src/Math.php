51130d56f99b904bddcf85a8b21f58368b38314d
Justin Campbell <campbell.justin.tyler@gmail.com>
2021-03-04 15:14:21 -0500

    Add parseLine and parseData functions

--- src/Math.php
+++ src/Math.php
@@ -57,4 +57,95 @@ namespace Sfp;
 
             return $data;
         }
+
+        /**
+         * Parses a singgle CSV line.
+         *
+         * @param string $line the line to parse
+         *
+         * @return array|bool array or false
+         */
+        private function parseLine($line = '')
+        {
+            if ('' === $line) {
+                return false;
+            }
+
+            // Break line into fields.
+            $fields = explode(',', $line);
+
+            // Check for correct number of fields.
+            if (3 != count($fields)) {
+                return false;
+            }
+
+            // Convert second field to a number:
+            // Check for special special case 0:
+            if ('0' === $fields[1]) {
+                $fields[1] = 0;
+            } elseif (0 !== intval($fields[1])) {
+                $fields[1] = intval($fields[1]);
+            } else {
+                return false;
+            }
+
+            // Convert third field to a boolean.
+            $stringValue = strtolower($fields[2]);
+            if ('true' === $stringValue) {
+                $fields[2] = true;
+            } elseif ('false' === $stringValue) {
+                $fields[2] = false;
+            } else {
+                return false;
+            }
+
+            // Build record.
+            return [
+                'key' => $fields[0],
+                'value' => $fields[1],
+                'accept' => $fields[2],
+            ];
+        }
+
+        /**
+         * Parses a CSV string into a multi-dimensional associative array.
+         *
+         * @param string $data CSV string to parse
+         *
+         * @return array|bool array or false if parsing failed
+         */
+        private function parseData($data = '')
+        {
+            if ('' === $data) {
+                return false;
+            }
+
+            // Detect newline character.
+            $crlf_exists = strpos($data, "\r\n");
+            $lf_exists = strpos($data, "\n");
+            // No line endings detected:
+            if (!($crlf_exists or $lf_exists)) {
+                return false;
+            }
+            // CRLF exists:
+            if ($crlf_exists and !$lf_exists) {
+                $newlineCharacter = "\r\n";
+            }
+            // LF exists:
+            else {
+                $newlineCharacter = "\n";
+            }
+
+            // Make array of reccords.
+            $records = explode($newlineCharacter, $data);
+
+            // Build array of reccords ignoring the first, title row and last, empty row.
+            $parsedData = [];
+            for ($i = 1; $i < count($records) - 1; ++$i) {
+                $parsedLine = $this->parseLine($records[$i]);
+                $parsedData[$i - 1] = $parsedLine;
+            }
+
+            return $parsedData;
+        }
     }
