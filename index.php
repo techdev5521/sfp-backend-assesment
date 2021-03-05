6d0914aeb8f7cda5a5c4ba1404233e338643fdc5
Justin Campbell <campbell.justin.tyler@gmail.com>
2021-03-04 23:45:13 -0500

    Output Sfp\Extend results

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ require_once 'src/Math.php';
 
 require_once 'src/Rotate.php';
 
+require_once 'src/Extend.php';
+
 $math = new Sfp\Math('assets/tabular.csv');
 echo "Result of Sfp\\Math->execute(): {$math->execute()}".PHP_EOL;
 
@@ -11,3 +13,9 @@ $rotate = new Sfp\Rotate(1, 'assets/rotate.json');
 $rotateResult = $rotate->execute();
 echo 'Result of Sfp\\Rotate->execute(): '.PHP_EOL;
 var_dump($rotateResult);
+echo PHP_EOL;
+
+$extend = new Sfp\Extend(1, 'assets/rotate.json');
+$extendResult = $extend->execute();
+echo 'Result of Sfp\\Extend->execute(): '.PHP_EOL;
+var_dump($extendResult);
